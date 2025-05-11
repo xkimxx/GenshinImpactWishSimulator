3801d1e2bcc07988219a363f42a60831160f639f
kimx <34332897+xkimxx@users.noreply.github.com>
2025-05-11 18:35:11 +0900

    Update GenshinImpactWishSimulator.py
    Updated

--- GenshinImpactWishSimulator.py
+++ GenshinImpactWishSimulator.py
@@ -1,10 +1,10 @@
-'''
+"""
 
-GenshinImpactWishSimulator 
+GenshinImpactWishSimulator
 Made_By_kimx
-2023/03/05
+2025/05/11
 
-'''
+"""
 
 from random import randint
 import matplotlib.pyplot as plt
@@ -12,176 +12,135 @@ import numpy as np
 
 #  FunctionDefinition
 
-NumberOfTrial=10000 # change the number if it takes longer
-WishType=True # True:Character,False:Weapons
-
-Constellation =0
-WishTotallPull=0
-AllSumTotalPull=0
-SumTotalPull=0
-NotPu=True
-MaxPull=0
-MinPull=0
-Temp=0
-
-ResultList=[]
+NumberOfTrial = 10000  # change the number if it takes longer
+WishType = True  # True:Character,False:Weapons
 
 
-def CharactarWish(NumberOfTrial,Constellation,WishTotallPull,SumTotalPull,NotPu,MaxPull,MinPull,Temp,WishType):
+class Wish:
+    def __init__(self, NumberOfTrial: int, WishType: bool):
+        self.NumberOfTrial = NumberOfTrial
+        self.Constellation = 0
+        self.WishTotallPull = 0
+        self.SumTotalPull = 0
+        self.MaxPull = 0
+        self.MinPull = 0
+        self.Temp = 0
+        self.ResultList = []
+        self.Lose = False
+        if WishType:
+            self.CaptureingRadiance = 0
+            self.Rate = 0.6
+            self.MaxConstellation = 7
+            self.StartIncrease = 73
+            self.IncreaseRate = 5.85
+            self.PickRate = 0.5
+        else:
+            self.CaptureingRadiance = None
+            self.Rate = 0.7
+            self.MaxConstellation = 5
+            self.StartIncrease = 62
+            self.IncreaseRate = 5.52
+            self.PickRate = 0.375
 
-    for i in range(NumberOfTrial):
-    
-        while Constellation < 8:
+    def wish(self, Rate, WishTotallPull, StartIncrease, IncreaseRate):
+        Wish = randint(0, 100000)
+        if (
+            Wish
+            <= (Rate) * 1000
+            - 1
+            + max(0, WishTotallPull - StartIncrease) * 1000 * IncreaseRate
+        ):
+            return True
+        else:
+            return False
 
-            WishTotallPull += 1
-            Wish = randint(0 , 100000)
-
-            if NotPu:
-
-                if Wish <= 599 + max (0,WishTotallPull-73) * 6000:
-                    #  50:50
-                    if randint(0,1) == 1:
-
-                        Constellation += 1
-                        SumTotalPull += 1
-                        WishTotallPull = 0
-
-                    else:
-
-                        NotPu=False
-                        SumTotalPull +=1
-                        WishTotallPull = 0
-
-                else:
-                    SumTotalPull += 1
-
-            else:
-
-                if Wish <= 599 + max(0,WishTotallPull-73) * 6000:
-
-                    NotPu = True
-                    Constellation += 1
-                    SumTotalPull += 1
-                    WishTotallPull = 0
-
-                else:
-                
-                    SumTotalPull+=1
-    
-        MaxPull=max(MaxPull,SumTotalPull-Temp)
-        MinPull=min(MinPull,SumTotalPull-Temp)
-
-        ResultList.append(SumTotalPull-Temp) #Get the value of pulling the Wish
-        Temp=SumTotalPull
-
-        Constellation=0
-
-    print(f"6C ave:{SumTotalPull/NumberOfTrial}primogem:{SumTotalPull/NumberOfTrial*160}")
-    print(f"1C ave:{SumTotalPull/NumberOfTrial/7}primogem:{SumTotalPull/NumberOfTrial*160/7}")
-
-    MakeGraph(MaxPull,MinPull,ResultList,WishType)
-
-def WeaponWish(NumberOfTrial,Constellation,WishTotallPull,SumTotalPull,NotPu,MaxPull,MinPull,Temp,WishType):
-
-    NotPu2=True
-
-    for i in range(NumberOfTrial):
-    
-        while Constellation < 6:
-
-            WishTotallPull += 1
-            Wish = randint(0 , 100000)
-
-            if NotPu and NotPu2:
-
-                if Wish <= 699 + max (0,WishTotallPull-62) * 7000:
-                    #  27.5%
-                    if randint(0,7) <= 2:
-
-                        Constellation += 1
-                        SumTotalPull += 1
-                        WishTotallPull = 0
-
-                    else:
-
-                        NotPu2=False
-                        SumTotalPull +=1
-                        WishTotallPull = 0
-
-                else:
-                    SumTotalPull += 1
-
-            elif NotPu:
-                    if Wish <= 699 + max (0,WishTotallPull-62) * 7000:
-                    #  50:50
-                        if randint(0,1) == 1:
-                            NotPu=True
-                            Constellation += 1
-                            SumTotalPull += 1
-                            WishTotallPull = 0
-
+    def all_wish(self):
+        for i in range(self.NumberOfTrial):
+            while self.Constellation <= self.MaxConstellation:
+                self.WishTotallPull += 1
+                if not self.Lose:
+                    if self.wish(
+                        self.Rate,
+                        self.WishTotallPull,
+                        self.StartIncrease,
+                        self.IncreaseRate,
+                    ):
+                        if (
+                            self.CaptureingRadiance is not None
+                            and self.CaptureingRadiance == 3
+                        ):
+                            self.Constellation += 1
+                            self.WishTotallPull = 0
                         else:
+                            if randint(0, 1000) <= self.PickRate * 1000:
+                                self.Constellation += 1
+                                self.WishTotallPull = 0
+                                if self.CaptureingRadiance is not None:
+                                    self.CaptureingRadiance = 0
 
-                            NotPu=False
-                            SumTotalPull +=1
-                            WishTotallPull = 0
+                            else:
+                                if self.CaptureingRadiance is not None:
+                                    self.CaptureingRadiance += 1
+                                self.Lose = True
+                                self.WishTotallPull = 0
 
-                    else:
-                        SumTotalPull += 1
-
-            else:
-
-                if Wish <= 699 + max(0,WishTotallPull-73) * 7000:
-
-                    NotPu = True
-                    NotPu2  = True
-                    Constellation += 1
-                    SumTotalPull += 1
-                    WishTotallPull = 0
+                    self.SumTotalPull += 1
 
                 else:
-                
-                    SumTotalPull+=1
+                    if self.wish(
+                        self.Rate,
+                        self.WishTotallPull,
+                        self.StartIncrease,
+                        self.IncreaseRate,
+                    ):
+                        self.Constellation += 1
+                        self.SumTotalPull += 1
+                        self.WishTotallPull = 0
 
-        MaxPull=max(MaxPull,SumTotalPull-Temp)
-        MinPull=min(MinPull,SumTotalPull-Temp)
+                    self.SumTotalPull += 1
 
-        ResultList.append(SumTotalPull-Temp) #Get the value of pulling the Wish
-        Temp=SumTotalPull
-
-        Constellation=0
-
-    print(f"R5 ave:{SumTotalPull/NumberOfTrial}primogem:{SumTotalPull/NumberOfTrial*160}")
-    print(f"R1 ave:{SumTotalPull/NumberOfTrial/5}primogem:{SumTotalPull/NumberOfTrial*160/5}")
-
-    MakeGraph(MaxPull,MinPull,ResultList,WishType)
+            self.MaxPull = max(self.MaxPull, self.SumTotalPull - self.Temp)
+            self.MinPull = min(self.MinPull, self.SumTotalPull - self.Temp)
+            self.ResultList.append(self.SumTotalPull - self.Temp)
+            self.Temp = self.SumTotalPull
+            self.Constellation = 0
+            self.Lose = False
+        return self.ResultList, self.MaxPull, self.MinPull
 
 
-def MakeGraph(MaxPull,MinPull,ResultList,WishType):
+class MakeGraph:
+    def __init__(self):
+        self.TotalPulList = []
+        self.RateList = []
 
-    TotalPulList = []
-    RateList = []
+    def makegraph(self, MaxPull, MinPull, ResultList, WishType):
+        for i in range(MinPull, MaxPull + 1):
+            self.TotalPulList.append(i)
 
-    for i in range(MinPull,MaxPull+1):
-        TotalPulList.append(i)
+        for i in range(MinPull, MaxPull + 1):
+            self.RateList.append(ResultList.count(i) / NumberOfTrial * 100)
 
-    for i in range(MinPull,MaxPull+1):
-        RateList.append(ResultList.count(i)/NumberOfTrial*100)
+        if WishType:
+            plt.title("C6")
+        else:
+            plt.title("R5")
 
-    if WishType:
-        plt.title("6C")
-    else:
-        plt.title("R5")
-        
-    plt.xlabel('Total Pull')
-    plt.ylabel('Rate(%)')
-    plt.bar(np.array(TotalPulList),np.array(RateList))
-    plt.show()
+        plt.xlabel("Total Pull")
+        plt.ylabel("Rate(%)")
+        plt.bar(np.array(self.TotalPulList), np.array(self.RateList))
+        plt.show()
+
 
 if __name__ == "__main__":
-    
+    w = Wish(NumberOfTrial, WishType)
+    ResultList, MaxPull, MinPull = w.all_wish()
     if WishType:
-        CharactarWish(NumberOfTrial,Constellation,WishTotallPull,SumTotalPull,NotPu,MaxPull,MinPull,Temp,WishType)
+        print(
+            f"C6 ave:{w.SumTotalPull / w.NumberOfTrial}primogem:{w.SumTotalPull / w.NumberOfTrial * 160}"
+        )
     else:
-        WeaponWish(NumberOfTrial,Constellation,WishTotallPull,SumTotalPull,NotPu,MaxPull,MinPull,Temp,WishType)
-
+        print(
+            f"R5 ave:{w.SumTotalPull / w.NumberOfTrial}primogem:{w.SumTotalPull / w.NumberOfTrial * 160}"
+        )
+    g = MakeGraph()
+    g.makegraph(MaxPull, MinPull, ResultList, WishType)
